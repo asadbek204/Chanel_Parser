be8a1c8137622aec742af10bf84fb913679aa227
Asadbek <ahikmatullayev024@gmail.com>
2023-07-16 12:50:11 +0500

    first release

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
-# from telethon.sync import TelegramClient
-# from config import API_ID, API_HASH
-
-# source_channel = 'allalamani'
-# target_channel = 'testlalala'
-
-# client = TelegramClient('session_name', API_ID, API_HASH)
-# client.start()
-# messages = client.iter_messages(source_channel)
-# for message in messages:
-#     client.send_message(target_channel, message.text)
-
-# client.disconnect()
-
-###############################################################
-
 import asyncio
 from telethon import TelegramClient
 from telethon import types
 from config import API_ID, API_HASH
-from pprint import pprint
 from time import sleep
 
-source_channel = 'allalamani'
+source_channel = 'sizbilannnn'
 target_channel = 'testlalala'
 client = TelegramClient('session_name', API_ID, API_HASH)
 
 async def main():
     await client.start()
     a = await client.get_messages(source_channel, limit=100000)
-    pprint(len(a))
     for message in a[::-1]:
-        pprint(message.text)
-        if message.text is not None and message.text != '':
-            await client.send_message(target_channel, message.text)
-        print(type(message))
-        print()
-        sleep(1)
-
+        if not isinstance(message, types.MessageService):
+            while True:
+                try:
+                    await client.send_message(target_channel, message)
+                except:
+                    sleep(1)
+                else:
+                    break
     await client.disconnect()
 asyncio.run(main())
